873bc53437d00003052a84cc865e2c1fc73f15d8
Pedro Perri <pedropgp@al.insper.edu.br>
2018-04-24 14:06:43 -0300

    Adicionado valor

--- EP.py
+++ EP.py
@@ -7,7 +7,6 @@ EP: Sistema de Grenciamento de Estoque
 @author: Renato Tajima e Pedro Perri
 """
 
-
 import json
 import os
 
@@ -21,14 +20,12 @@ with open('estoque.json', 'r') as f:
         conteudo = f.read()
         estoque = json.loads(conteudo)
         
-        
-        
-        
 escolha = -1
 
+print ('\nCONTROLE DE ESTOQUE ABERTO')
+
 while escolha != 0:
-    print ('\nControle de estoque')
-    print ('0 - sair ')
+    print ('\n0 - sair ')
     print ('1 - adicionar item')
     print ('2 - remover item')
     print ('3 - alterar item')
@@ -42,8 +39,13 @@ while escolha != 0:
             print('\nEste produto já existe no estoque!')
         else:
             quantidade = int(input('Quantidade inicial: '))
-            estoque[produto] = {'quantidade' : quantidade} 
-            print ('\nItem adicionado com sucesso!')
+            preco = float(input('Preço do produto: R$ '))
+            if preco >= 0:
+                estoque[produto] = {'quantidade': quantidade,'valor' : preco}
+                print ('\nItem adicionado com sucesso!')
+            else:
+                print('\nValor inválido!')
+            
             
     elif escolha == 2:
         produto = input('Nome do produto: ')
@@ -52,29 +54,60 @@ while escolha != 0:
             print ('\nO item foi removido com sucesso!')
         else:
             print ('\nO produto não foi encontrado no estoque!')
+            
     
     elif escolha == 3:
         produto = input('Nome do produto: ')    
         if produto in estoque:
+            comando = -1
             print ('\nA atual quantidade deste produto no estoque é: {0}'\
                    .format(estoque[produto]['quantidade']))
-            quantidade = int(input('Quantidade a ser adicionada: '))
-            estoque[produto]['quantidade'] += quantidade
-            print('\nA nova quantidade deste produto no estoque é: {0}'\
+            print ('\nO atual preço deste produto é: R${0}'\
+                   .format(estoque[produto]['valor']))
+            print('\n7 - Alterar quantidade do produto no estoque')
+            print('8 - Alterar preço do produto')
+            print('9 - Alterar preço e quantidade do produto')
+            comando = int(input('\nEscolha um comando: '))
+            if comando ==7:
+                quantidade = int(input('\nQuantidade a ser adicionada: '))
+                estoque[produto]['quantidade'] += quantidade
+                print('\nA nova quantidade deste produto no estoque é: {0}'\
                   .format(estoque[produto]['quantidade']))
+            elif comando == 8:
+                novo_preco = float(input('\nNovo preço do produto: R$ '))
+                if novo_preco >= 0:
+                    estoque[produto]['valor'] = novo_preco
+                    print('\nO novo preço deste produto é: R${0}'\
+                          .format(estoque[produto]['valor']))
+                else:
+                    print('\nValor inválido')
+            
+            elif comando == 9:
+                quantidade = int(input('\nQuantidade a ser adicionada: '))
+                novo_preco = float(input('\nNovo preço do produto: R$ '))
+                estoque[produto]['quantidade'] += quantidade
+                if novo_preco >= 0:
+                    estoque[produto]['valor'] = novo_preco
+                    print('\nA nova quantidade deste produto no estoque é: {0}'\
+                          .format(estoque[produto]['quantidade']))
+                    print('\nO novo preço deste produto é: R${0}'\
+                          .format(estoque[produto]['valor']))
+                else:
+                    print('\nValor inválido')
         else:
             print ('\nO produto não foi encontrado no estoque!')
     
     elif escolha == 4:
         print (estoque)
     
+    
     elif escolha != 0:
         print('\nComando inválido!')
         
             
 if escolha == 0:
-    print ('\nControle de estoque encerrado!')
-    print ('\nAté mais!')   
+    print ('\nCONTROLE DE ESTOQUE ENCERRADO')
+    print ('\nATÉ MAIS!')   
     novo_estoque = json.dumps(estoque, sort_keys = True, indent = 0)
     with open ('estoque.json', 'w') as f:
         novo_conteudo = f.write(novo_estoque)
